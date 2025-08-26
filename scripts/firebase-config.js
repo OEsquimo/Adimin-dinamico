50a18fb443c6076ff1d387522563ec7449d44540
Oesquimno <wellingtonferreiramelo27@gmail.com>
2025-08-25 22:33:03 -0300

    Update firebase-config.js

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,1 +1,14 @@
+// Configuração do Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCFf5gckKE6rg7MFuBYAO84aV-sNrdY2JQ",
+    authDomain: "agendamento-esquimo.firebaseapp.com",
+    projectId: "agendamento-esquimo",
+    storageBucket: "agendamento-esquimo.appspot.com",
+    messagingSenderId: "348946727206",
+    appId: "1:348946727206:web:f5989788f13c259be0c1e7"
+};
 
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
